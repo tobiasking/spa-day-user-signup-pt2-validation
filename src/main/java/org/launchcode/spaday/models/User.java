186abe7b1d7e2bbada634f619ad79b2a6eaf6734
toby king <tobyjacquesking@gmail.com>
2022-09-01 20:28:08 -0400

    bonus missions

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -5,38 +5,34 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-public class User {
-    private String username;
-    private String email;
-    private String password;
-
     public class User {
 
+        @NotBlank
+        @Size(min = 5, max = 15)
+        private String username;
+
+        @Email
+        private String email;
+
+        @NotBlank
+        @Size(min = 6)
+        private String password;
+
+        @NotNull(message = "Passwords do not match.")
+        @NotBlank
+        @Size(min = 6)
+        private String verifyPassword;
 
         public User() {
 
-            @NotBlank
-            @Size(min = 5, max = 15)
-            private String username;
-
-            @Email
-            private String email;
-
-            @NotBlank
-            @Size(min = 6)
-            private String password;
-
-            @NotNull(message = "Passwords do not match.")
-            @NotBlank
-            @Size(min = 6)
-            private String verifyPassword;
         }
 
-        public User(String username, String email, String password) {
+        public User(String username, String email, String password, String verifyPassword) {
             this();
             this.username = username;
             this.email = email;
             this.password = password;
+            this.verifyPassword = verifyPassword;
         }
 
         public String getUsername() {
@@ -61,11 +57,23 @@ public class User {
 
         public void setPassword(String password) {
             this.password = password;
+            checkPassword();
         }
 
+        public String getVerifyPassword() {
+            return verifyPassword;
+        }
 
+        public void setVerifyPassword(String verifyPassword) {
+            this.verifyPassword = verifyPassword;
+            checkPassword();
+        }
+
+        private void checkPassword() {
+            if (!this.password.equals(this.verifyPassword)){
+                this.verifyPassword = null;
+            }
+        }
     }
-
-}
 
 
